22db058f176db41dfbd989c06ad58e40a3c4bfd9
Tyler Fahey <twfahey1@gmail.com>
2019-09-04 18:45:01 -0500

    Adding initial ParserService

---
+++ web/modules/custom/ultimate_lexparser/src/ParserService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\ultimate_lexparser;
+
+/**
+ * Class ParserService.
+ */
+class ParserService implements ParserServiceInterface {
+
+  /**
+   * Constructs a new ParserService object.
+   */
+  public function __construct() {
+
+  }
+
+}
---
+++ web/modules/custom/ultimate_lexparser/src/ParserServiceInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\ultimate_lexparser;
+
+/**
+ * Interface ParserServiceInterface.
+ */
+interface ParserServiceInterface {
+
+
+}
